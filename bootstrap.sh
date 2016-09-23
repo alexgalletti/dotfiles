08d6355996cc0c55702aae85aad835faabed6a42
Alex Galletti <agalletti@bluechipww.com>
2016-09-23 10:06:31 -0500

    updated dotfiles. removed sublime and terminal configs

--- bootstrap.sh
+++ bootstrap.sh
@@ -33,7 +33,7 @@ fi;
 read -p "Would you like to install/overwrite dotfiles? (y/n) " -n 1;
 echo "";
 if [[ $REPLY =~ ^[Yy]$ ]]; then
-    rsync --exclude ".git/" --exclude "Mac/" --exclude ".DS_Store" --exclude ".osx" --exclude "bootstrap.sh" --exclude "readme.markdown" -avh --no-perms . ~;
+    rsync --exclude ".git/" --exclude ".DS_Store" --exclude ".osx" --exclude "bootstrap.sh" --exclude "readme.md" -avh --no-perms . ~;
 else
     echo "No changes have been made, exiting.";
 fi;
---
+++ readme.md
@@ -0,0 +1,2 @@
+## Dotfiles ##
+Run `./bootstrap.sh`
